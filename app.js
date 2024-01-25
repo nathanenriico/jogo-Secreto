d5ae6587746b1c67961a1fbe6dd37e93138347b9
nathanenriico <147453169+nathanenriico@users.noreply.github.com>
2024-01-25 14:23:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo Numero Secreto';
+
+let texto = document.querySelector('p');
+texto.innerHTML = 'Digite um numero entre 1 a 10'
+
+ function verificarChute() {
+    console.log('botão foi clicado')
+}
